4204cf67d64e1fd5509e1d002b019401f746aff0
Jacob Chae <jbcden@gmail.com>
2015-01-14 11:46:44 -0500

    write inital failing test for RageFetcher

--- test/time_cards/hours_calculator_test.exs
+++ test/time_cards/hours_calculator_test.exs
@@ -15,16 +15,4 @@ defmodule TimeCards.HoursCalculatorTest do
 
     assert result == 4.75
   end
-
-  test "it can sum entries for a given range" do
-    e1 = %Entry{time_start: {{2015, 1, 13}, {12, 0,0}}, time_end: {{2015, 1, 13}, {14, 0, 0}}}
-    e2 = %Entry{time_start: {{2015, 1, 23}, {12, 0,0}}, time_end: {{2015, 1, 23}, {14, 45, 0}}}
-    e3 = %Entry{time_start: {{2015, 2, 23}, {12, 0,0}}, time_end: {{2015, 1, 23}, {14, 45, 0}}}
-    beginning = {{2015, 1, 12}, {12, 0, 0}}
-    range_end = {{2015, 1, 24}, {12, 0, 0}}
-    result = TimeCards.HoursCalculator.calculate([e1,e2], beginning, range_end)
-
-    assert result == 4.75
-
-  end
 end
